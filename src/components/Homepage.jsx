86b3cb58dba98a13d24282cc30380c919bc81144
babucarr32 <baboucarry@gmail.com>
2023-05-16 21:29:22 +0000

    Made the font and bg color to states

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function Homepage() {
+  const [fontColor, setFontColor] = useState("black");
+  const [bgColor, setBgColor] = useState("white");
+
+  return (
+    <div
+      className={`bg-${bgColor} shadow-lg shadow-black w-100 h-90vh rounded-3xl`}
+    >
+      <h1 className={`text-${fontColor} `}>Hello World</h1>
+      <h1>Hsssse</h1>
+    </div>
+  );
+}
+
+export default Homepage;
