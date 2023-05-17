ff0a17eea46d24db22245511187fa4e13ed4b727
babucarr32 <baboucarry@gmail.com>
2023-05-17 14:53:45 +0000

    Saved item to LS

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Input from "./Input";
 import TextArea from "./TextArea";
 import Title from "./Title";
 import Button from "./Button";
 
 function AddTask() {
-  const handleCreateTodo = (e) => {
-    e.preventDefault();
-    const time = e.target.time.value;
-    const title = e.target.title.value;
-    const comment = e.target.comment.value;
+  let handleCreateTodo;
 
-    console.log(time, title, comment);
-  };
+  useEffect(() => {
+    handleCreateTodo = (e) => {
+      e.preventDefault();
+      const time = e.target.time.value;
+      const title = e.target.title.value;
+      const comment = e.target.comment.value;
+      localStorage.setItem(
+        "TODO",
+        JSON.stringify({
+          name: name,
+          time: time,
+          comment: comment,
+        })
+      );
+      console.log(time, title, comment);
+    };
+  }, []);
 
   return (
     <div className="addTask w-70vh bg-white absolute z-10 shadow-2xl p-3 rounded-2xl">
-      <form action="" onSubmit={handleCreateTodo}>
+      <form action="" onSubmit={(e) => handleCreateTodo(e)}>
         <Title />
         <Input placeholder={"Time: 09:30 AM"} name={"time"} />
         <Input placeholder={"Title"} name={"title"} />
