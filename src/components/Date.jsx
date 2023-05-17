65abfac5814e0018ec89f6291e720c373b19f87c
babucarr32 <baboucarry@gmail.com>
2023-05-17 07:59:49 +0000

    Made the date color gray

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -1,28 +1,43 @@
 import React from "react";
 
 function Date() {
+  const color = "gray";
   return (
-    <ul className="flex justify-center text-5xl font-bold gap-10 p-10">
-      <li>
-        <p>07</p>
+    <ul className="flex justify-around text-5xl font-bold gap-2 pt-5 pb-5">
+      <li className="flex flex-col justify-center items-center w-100">
+        <p className={`text-zinc-300`}>07</p>
+        <p className={`text-zinc-300 text-xl`}>mon</p>
+        <div className="h-5 w-1 bg-zinc-300"></div>
       </li>
-      <li>
-        <p>08</p>
+      <li className="flex flex-col justify-center items-center w-100">
+        <p className={`text-zinc-300`}>08</p>
+        <p className={`text-zinc-300 text-xl`}>tue</p>
+        <div className="h-5 w-1 bg-zinc-300"></div>
       </li>
-      <li>
-        <p>09</p>
+      <li className="flex flex-col justify-center items-center w-100">
+        <p className={``}>09</p>
+        <p className={` text-xl`}>wed</p>
+        <div className="h-5 w-1 bg-black"></div>
       </li>
-      <li>
-        <p>10</p>
+      <li className="flex flex-col justify-center items-center w-100">
+        <p className={`text-zinc-300`}>10</p>
+        <p className={`text-zinc-300 text-xl`}>thu</p>
+        <div className="h-5 w-1 bg-zinc-300"></div>
       </li>
-      <li>
-        <p>11</p>
+      <li className="flex flex-col justify-center items-center w-100">
+        <p className={`text-zinc-300`}>11</p>
+        <p className={`text-zinc-300 text-xl`}>fri</p>
+        <div className="h-5 w-1 bg-zinc-300"></div>
       </li>
-      <li>
-        <p>12</p>
+      <li className="flex flex-col justify-center items-center w-100">
+        <p className={`text-zinc-300`}>12</p>
+        <p className={`text-zinc-300 text-xl`}>sat</p>
+        <div className="h-5 w-1 bg-zinc-300"></div>
       </li>
-      <li>
-        <p>13</p>
+      <li className="flex flex-col justify-center items-center w-100">
+        <p className={`text-zinc-300`}>13</p>
+        <p className={`text-zinc-300 text-xl`}>sun</p>
+        <div className="h-5 w-1 bg-zinc-300"></div>
       </li>
     </ul>
   );
--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -6,13 +6,14 @@ function TaskContainer() {
   return (
     <div className="flex flex-col  gap-2 w-100 justify-between">
       <DateComponent />
-      <div className="flex gap-2 items-end pl-2 pr-2 w-100 h-100 overflow-hidden">
+      <div className="flex gap-2 items-end pl-2 pr-2 h-100 overflow-hidden">
         <Task height="h-100" width="w-100" bgColor={"bg-purple-400"} />
         <Task height="h-70vh" width="w-100" bgColor={"bg-yellow-400"} />
         <Task height="h-60vh" width="w-100" bgColor={"bg-cyan-400"} />
         <Task height="h-60" width="w-100" bgColor={"bg-pink-400"} />
         <Task height="h-96" width="w-100" bgColor={"bg-red-400"} />
         <Task height="h-60vh" width="w-100" bgColor={"bg-cyan-400"} />
+        <Task height="h-60" width="w-100" bgColor={"bg-pink-400"} />
       </div>
     </div>
   );
