dd75ab7808b3cac2280e02a57495429a6abfe5a6
babucarr32 <baboucarry@gmail.com>
2023-05-17 16:11:20 +0000

    Made the TaskContainer to listen to on create todo

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -1,22 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Task from "./Task";
 import DateComponent from "./DateComponent";
+import AddTask from "./AddTask";
+import { ThemeContext } from "./Homepage";
 
 function TaskContainer() {
   const [getTodoData, setGetTodoData] = useState([]);
+  const { addTask } = useContext(ThemeContext);
 
   useEffect(() => {
     const getLocalStorage = localStorage.getItem("TODO");
     if (getLocalStorage !== null) {
       setGetTodoData([...JSON.parse(getLocalStorage)]);
     }
-  }, []);
+  }, [addTask]);
 
   return (
     <div className="flex flex-col  gap-2 w-100 justify-between">
       <DateComponent />
       <div className="flex gap-2 items-end pl-2 pr-2 h-100">
-        <Task height="h-100" width="w-100" bgColor={"bg-purple-200"} />
         {getTodoData.map((data) => (
           <Task height="h-70vh" width="w-100" bgColor={"bg-yellow-200"} />
         ))}
