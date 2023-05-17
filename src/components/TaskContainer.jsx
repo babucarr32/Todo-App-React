cc4ed95336a50a5865c62e762d1f091200e3cdca
babucarr32 <baboucarry@gmail.com>
2023-05-17 16:08:37 +0000

    Making the TaskConatiner dynamic

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Task from "./Task";
 import DateComponent from "./DateComponent";
 
 function TaskContainer() {
+  const [getTodoData, setGetTodoData] = useState([]);
+
+  useEffect(() => {
+    const getLocalStorage = localStorage.getItem("TODO");
+    if (getLocalStorage !== null) {
+      setGetTodoData([...JSON.parse(getLocalStorage)]);
+    }
+  }, []);
+
   return (
     <div className="flex flex-col  gap-2 w-100 justify-between">
       <DateComponent />
       <div className="flex gap-2 items-end pl-2 pr-2 h-100">
         <Task height="h-100" width="w-100" bgColor={"bg-purple-200"} />
-        <Task height="h-70vh" width="w-100" bgColor={"bg-yellow-200"} />
-        <Task height="h-60vh" width="w-100" bgColor={"bg-cyan-200"} />
-        <Task height="h-60" width="w-100" bgColor={"bg-pink-200"} />
-        <Task height="h-96" width="w-100" bgColor={"bg-red-200"} />
-        <Task height="h-60vh" width="w-100" bgColor={"bg-cyan-200"} />
-        <Task height="h-60" width="w-100" bgColor={"bg-pink-200"} />
+        {getTodoData.map((data) => (
+          <Task height="h-70vh" width="w-100" bgColor={"bg-yellow-200"} />
+        ))}
       </div>
     </div>
   );
