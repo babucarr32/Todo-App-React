cfeed9d61db286f651ec1447501196a7d7282976
babucarr32 <baboucarry@gmail.com>
2023-05-17 14:35:52 +0000

    Created handle form submit func

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -5,9 +5,14 @@ import Title from "./Title";
 import Button from "./Button";
 
 function AddTask() {
+  const handleCreateTodo = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
     <div className="addTask w-70vh bg-white absolute z-10 shadow-2xl p-3 rounded-2xl">
-      <form action="">
+      <form action="" onSubmit={handleCreateTodo}>
         <Title />
         <Input placeholder={"Time: 09:30 AM"} />
         <Input placeholder={"Title"} />
